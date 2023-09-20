559e427099a58db352a1f8b482998213cfe1bf1d
Marcos-Aquin0 <143093657+Marcos-Aquin0@users.noreply.github.com>
2023-09-20 10:19:24 -0300

    teste para mover uma bike não registrada

--- src/app.spec.ts
+++ src/app.spec.ts
@@ -17,7 +17,7 @@ describe('App', () => {
         const hour = 1000 * 60 * 60
         clock.tick(2 * hour)
         const rentAmount = app.returnBike(bike.id, user.email)
-        expect(rentAmount).toEqual(200.0)
+        expect.arguments(rentAmount).toEqual(200.0)
     })
 
     it('should be able to move a bike to a specific location', () => {
@@ -27,13 +27,36 @@ describe('App', () => {
         app.registerBike(bike)
         const newYork = new Location(40.753056, -73.983056)
         app.moveBikeTo(bike.id, newYork)
-        expect(bike.location.latitude).toEqual(newYork.latitude)
-        expect(bike.location.longitude).toEqual(newYork.longitude)
+        expect.arguments(bike.location.latitude).toEqual(newYork.latitude)
+        expect.arguments(bike.location.longitude).toEqual(newYork.longitude)
     })
 
     it('should throw an exception when trying to move an unregistered bike', () => {
+        const app = new App()
+        const newYork = new Location(40.753056, -73.983056)
+        const bike = new Bike('caloi mountainbike', 'mountain bike',
+        1234, 1234, 100.0, 'My bike', 5, []);
+        const bikeId = '123';
+
+        // let error: Error | null = null;
+        // try {
+        //     app.moveBikeTo(bikeId, newYork);
+        // } catch (e) {
+        // error = e as Error;
+        // }
+
+        // if (error !== null) {
+        //     console.error(error);
+        // }
+
+        const moveUnregisteredBike = () => {
+            app.moveBikeTo(bikeId, newYork);
+          };
+          expect.arguments(moveUnregisteredBike).toThrowError(`Bike ${bikeId} is not registered.`);
+        });
+
+
     })
-})
 
 function expect(longitude: number) {
     throw new Error("Function not implemented.")
